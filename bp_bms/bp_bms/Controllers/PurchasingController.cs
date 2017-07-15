37e445da561038e52cf9c726dd9adf819b742d85
hafizSiddiq7675 <hafiz.siddiq2k17@gmail.com>
2017-07-15 10:59:13 -0700

    Work till 15/07/2017

---
+++ bp_bms/bp_bms/Controllers/PurchasingController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using bp_bms.Models;
+
+namespace bp_bms.Controllers
+{
+    public class PurchasingController : Controller
+    {
+        private bmsEntitiesPurchasing db = new bmsEntitiesPurchasing();
+
+        // GET: /Purchasing/
+        public ActionResult Index()
+        {
+            return View(db.vendors.ToList());
+        }
+
+        // GET: /Purchasing/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            vendor vendor = db.vendors.Find(id);
+            if (vendor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vendor);
+        }
+
+        // GET: /Purchasing/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Purchasing/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="id,vid,accID,fname,lname,address,phone,Email,BankName,BankAcTitle,BankAcNo")] vendor vendor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.vendors.Add(vendor);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(vendor);
+        }
+
+        // GET: /Purchasing/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            vendor vendor = db.vendors.Find(id);
+            if (vendor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vendor);
+        }
+
+        // POST: /Purchasing/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="id,vid,accID,fname,lname,address,phone,Email,BankName,BankAcTitle,BankAcNo")] vendor vendor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(vendor).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(vendor);
+        }
+
+        // GET: /Purchasing/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            vendor vendor = db.vendors.Find(id);
+            if (vendor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vendor);
+        }
+
+        // POST: /Purchasing/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            vendor vendor = db.vendors.Find(id);
+            db.vendors.Remove(vendor);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ bp_bms/bp_bms/Models/vendorViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace bp_bms.Models
+{
+    public class vendorViewModel
+    {
+        //Differnce between Display and Display Name, Why I m Using Display(Name="..")
+        //Add Velidations
+        [Key]
+        public int id { get; set; }
+        [Display(Name="Vendor Identification")]
+        [Required]
+        public string vid { get; set; }
+        [Display(Name="Account ID")]
+        [Required]
+        public string accID { get; set; }
+        [Display(Name="First Name")]
+        [Required]
+        public string fname { get; set; }
+        [Display(Name="Last Name")]
+        [Required]
+        public string lname { get; set; }
+        [Display(Name="Postal Address")]
+        [Required]
+        public string address { get; set; }
+        [Display(Name="Contact Number")]
+        [Required]
+        public string phone { get; set; }
+        [Display(Name="Email Address")]
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Display(Name = "Select Bank Account")]
+        public string BankName { get; set; }
+        [Display(Name="Bank Account Title of Vendor")]
+        public string BankAcTitle { get; set; }
+        [Display(Name="Bank Account Number")]
+        public string BankAcNo { get; set; }
+    }
+}
